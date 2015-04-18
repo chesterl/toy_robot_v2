a8b39fedf89fe4ce1d70a64aad91a83c46b38f60
Chester Lam <chesterlam620@gmail.com>
2015-04-18 14:30:50 -0700

    Added main toy_robot file to read commands

--- bin/toy_robot_simulation.rb
+++ bin/toy_robot_simulation.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/env ruby
-# require library files here
+require_relative '../lib/simulation'
+require_relative '../lib/table'
+require_relative '../lib/robot'
+require_relative '../lib/table_position'
+require_relative '../lib/direction'
+require 'pry'
 
+command = ''
+simulation = Simulation.new
+puts 'Enter a Command'
+puts 'You must issue a "PLACE X,Y,DIRECTION" command followed by any of the following: "LEFT", "RIGHT", "MOVE", "REPORT", "PLACE X,Y,DIRECTION" '
+puts 'Type "exit" to close program'
 
+while command != "exit\n"
+  command = ARGF.readline
+  simulation.process(command)
+end
+
+puts 'Program Terminated. Goodbye.'
