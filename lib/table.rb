2246ad8eeb5fe848503d584512a0d3ca8409d848
Chester Lam <chesterlam620@gmail.com>
2015-04-18 23:00:40 -0700

    Check for valid range in Table Class

--- lib/table.rb
+++ lib/table.rb
@@ -1,8 +1,12 @@
 class Table
 
   def initialize(width,height)
-    @width = width
-    @height = height
+    @width = 0..width
+    @height = 0..height
+  end
+
+  def valid_position?(x,y)
+    @width.include?(x) and @height.include?(y)
   end
 
 end
