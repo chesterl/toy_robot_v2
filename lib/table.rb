318bfd9b081237a904a41aee9c35bda61dbf6d9c
Chester Lam <chesterlam620@gmail.com>
2015-05-14 21:08:15 -0700

    Wrapped instance variables with attr_accessor

--- lib/table.rb
+++ lib/table.rb
@@ -1,12 +1,14 @@
 class Table
 
+  attr_reader :width, :height
+
   def initialize(width = 5,height = 5)
     @width = 0..width
     @height = 0..height
   end
 
   def valid_position?(x,y)
-    (@width).include?(x) and (@height).include?(y)
+    width.include?(x) and height.include?(y)
   end
 
 end
